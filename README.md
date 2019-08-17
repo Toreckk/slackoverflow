6a4db3233ffa20fa8fcc002d2030a57fe0be4fb6
Juan Coret <coret.juan@gmail.com>
2019-08-17 19:11:36 +0200

    added callback

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # slackoverflow
- A stack overflow integration for slack in nodejs
+
+A stack overflow integration for slack in nodejs
+
+```html
+<a
+  href="https://slack.com/oauth/authorize?client_id=722401595268.711448528707&scope=commands"
+  ><img
+    alt="Add to Slack"
+    height="40"
+    width="139"
+    src="https://platform.slack-edge.com/img/add_to_slack.png"
+    srcset="
+      https://platform.slack-edge.com/img/add_to_slack.png    1x,
+      https://platform.slack-edge.com/img/add_to_slack@2x.png 2x
+    "
+/></a>
+```
--- handler.js
+++ handler.js
@@ -43,17 +43,16 @@ module.exports.overflow = async event => {
   return {
     statusCode: 200,
     body: resp
-    /*body: JSON.stringify(
-      {
-        message: "Success!",
-        input: querystring.parse(event.body).text,
-        res: resp
       },
-      null,
-      2
-    )*/
   };
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.authorization = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Authorization called!"
+    })
+  };
+  callback(null, response);
 };
