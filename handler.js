e7867c78d3a1916eaee1c0c7f867ff5b4bca6ba9
Juan Coret <coret.juan@gmail.com>
2019-08-17 17:19:23 +0200

    returns top 5 results from stackoverflow

--- handler.js
+++ handler.js
@@ -1,13 +1,33 @@
 "use strict";
 const querystring = require("querystring");
+const axios = require("axios");
 
-module.exports.hello = async event => {
+const StackOverflowURI = (title, sort = "votes", order = "desc") => {
+  const baseURL = "https://api.stackexchange.com/2.2/search/advanced?";
+  const query = querystring.stringify({
+    order,
+    sort,
+    q: title,
+    site: "stackoverflow",
+    filter: "!-NA3i5zsvRkKYaEQLQcSR3s.llhvkX.IY"
+  });
+
+  const searchURL = baseURL + query;
+
+  return searchURL;
+};
+
+module.exports.overflow = async event => {
+  let text = querystring.parse(event.body).text;
+  let { data } = await axios.get(StackOverflowURI(text));
+  let res = data.items.filter((el, i) => i < 5);
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: "Success!",
-        input: querystring.parse(event.body).text
+        input: querystring.parse(event.body).text,
+        res: res
       },
       null,
       2
